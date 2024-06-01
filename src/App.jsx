874fec105e69c9294fca3fc0f15bf2968cb9bbf9
f-parsa <76214676+f-parsa@users.noreply.github.com>
2024-06-01 14:16:16 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <nav className='navbar'>
+        navbar Content
+      </nav>
+      <main>Main Content</main>
+    </div>
+  )
+}
+
+export default App
